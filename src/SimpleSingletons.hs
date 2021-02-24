f2060e05c38767e9f452e29d719ae162eca6ed53
Markus Dangl <markus.r.dangl@gmail.com>
2021-02-24 22:45:02 +0100

    I wanted to collapse a hallway, but instead GHC collapsed.

--- src/SimpleSingletons.hs
+++ src/SimpleSingletons.hs
@@ -25,23 +25,26 @@ Use ":k!" to evaluate types:
         this helps when the pattern exhaustion check fails on pattern synonyms    
 -}
 
--- https://blog.jle.im/entry/introduction-to-singletons-1.html
+-- https://blog.jle.im/entries/series/+introduction-to-singletons.html
 -- https://www.youtube.com/watch?v=ZoygbBiLo-w
 -- https://talks.jle.im/lambdaconf-2017/singletons/singleton-slides.html
 
 module SimpleSingletons where
 
 import Data.Kind    ( Type )
+import Data.Void    ( absurd )
 
 import Data.Singletons
 import Data.Singletons.TH
 import Data.Singletons.Prelude
+--import Data.Singletons.Decide
 
 {--------------------------------------------------------------------------------------------------}
+-- Part 1: https://blog.jle.im/entry/introduction-to-singletons-1.html
 
 $(singletons [d|
     data DoorState = Opened | Closed | Locked
-        deriving (Show, Eq)
+        deriving (Show, Eq, Ord)
     |])
 -- generates:
 -- data Sing :: ... SOpened, SClosed, SLocked
@@ -230,6 +233,7 @@ openAnyDoor i = case sing @s of
     SLocked -> fmap openDoor . unlockDoor i
 
 {--------------------------------------------------------------------------------------------------}
+-- Part 2: https://blog.jle.im/entry/introduction-to-singletons-2.html
 
 data SomeDoor where
     MkSomeDoor :: Sing s -> Door s -> SomeDoor
@@ -334,3 +338,292 @@ openAnySomeDoor i sd@(MkSomeDoor Sing d) =      -- Mecker mecker.
 -- other file because of name clashes
 
 {--------------------------------------------------------------------------------------------------}
+-- Part 3: https://blog.jle.im/entry/introduction-to-singletons-3.html
+
+data Knockable :: DoorState -> Type where
+    KnockClosed :: Knockable 'Closed
+    KnockLocked :: Knockable 'Locked
+
+class Proved p a where
+    auto :: p a
+
+instance Proved Knockable 'Closed where
+    auto = KnockClosed
+
+instance Proved Knockable 'Locked where
+    auto = KnockLocked
+
+knock :: Knockable s -> Door s -> IO ()
+knock _ d = putStrLn $ "Knock knock on " ++ doorMaterial d ++ " door!"
+
+-- The compiler generates the proof
+knockMyDoor0 = knock auto myDoor0
+
+{--------------------------------------------------------------------------------------------------}
+
+{-
+-- | 'a' cannot exist.  Commonly also called `Not`
+type Refuted' a = a -> Void
+
+data Decision' a = Proved' a                -- ^ a value of a exists
+                 | Disproved' (Refuted' a)  -- ^ a value of a cannot exist
+-}
+
+isKnockable :: Sing s -> Decision (Knockable s)
+isKnockable SOpened = Disproved (\case {})
+--isKnockable _       = Proved auto             -- No instance for (Proved Knockable s)
+isKnockable SClosed = Proved KnockClosed
+isKnockable SLocked = Proved KnockLocked
+
+knockSomeDoor :: SomeDoor -> IO ()
+knockSomeDoor (MkSomeDoor s d) = case isKnockable s of
+    Proved k        -> knock k d
+    Disproved _     -> putStrLn $ "Can't knock on an " ++ show (doorStatus' s d) ++ " door"
+
+knockMyDoor1 = knockSomeDoor myDoor1
+
+{--------------------------------------------------------------------------------------------------}
+-- Type Level Functions
+
+$(singletons [d|
+    data Pass = Obstruct | Allow
+        deriving (Show, Eq, Ord)
+    |])
+
+{- -- Hand-written
+type family StatePass (s :: DoorState) :: Pass where
+    StatePass 'Opened = 'Allow
+    StatePass 'Closed = 'Obstruct
+    StatePass 'Locked = 'Obstruct
+
+sStatePass :: Sing s -> Sing (StatePass s)
+sStatePass = \case
+    SOpened -> SAllow
+    SClosed -> SObstruct
+    SLocked -> SObstruct
+-}
+
+-- Using the library
+$(singletons [d|
+  statePass :: DoorState -> Pass
+  statePass Opened = Allow
+  statePass Closed = Obstruct
+  statePass Locked = Obstruct
+  |])
+
+knockP :: (StatePass s ~ 'Obstruct) => Door s -> IO ()
+knockP d = putStrLn $ "Knock knock on " ++ doorMaterial d ++ " door!"
+
+-- The compiler generates the proof
+knockMyDoor01 = knockP myDoor0
+-- Error: ouldn't match type ‘'Allow’ with ‘'Obstruct’
+--knockMyDoor02 = knockP $ openDoor myDoor0
+
+knockSomeDoorP :: SomeDoor -> IO ()
+knockSomeDoorP (MkSomeDoor s d) = case sStatePass s of
+    SObstruct   -> knockP d
+    SAllow      -> putStrLn $ "Can't knock on an " ++ show (doorStatus' s d) ++ " door"
+
+{--------------------------------------------------------------------------------------------------}
+-- Exercises for Pt. 3
+
+isDoorState :: Sing (s :: DoorState) -> Decision (SDoorState s)
+isDoorState = Proved
+
+refuteRefuteKnockable
+    :: forall s. SingI s
+    => Refuted (Refuted (Knockable s))
+    -> Knockable s
+refuteRefuteKnockable (f :: (Knockable s -> Void) -> Void)
+    = case isKnockable (sing @s) of
+        Proved x    -> x
+        Disproved a -> absurd $ f a
+
+-- a
+data And' :: (k -> Type) -> (k -> Type) -> (k -> Type) where
+    And' :: p a -> q a -> And' p q a
+
+-- b
+data Or' :: (k -> Type) -> (k -> Type) -> (k -> Type) where
+    Or'L :: p a -> Or' p q a
+    Or'R :: q a -> Or' p q a
+
+-- c
+decideAnd
+    :: (forall x. Sing x -> Decision (p x))
+    -> (forall x. Sing x -> Decision (q x))
+    -> Sing a
+    -> Decision (And' p q a)
+decideAnd l r s = case (l s, r s) of
+    (Proved t1, Proved t2)  -> Proved (And' t1 t2)
+    (Disproved a1, _     )  -> Disproved $ \(And' pa _) -> a1 pa
+    (_     , Disproved a2)  -> Disproved $ \(And' _ pb) -> a2 pb
+
+
+decideOr
+    :: (forall x. Sing x -> Decision (p x))
+    -> (forall x. Sing x -> Decision (q x))
+    -> Sing a
+    -> Decision (Or' p q a)
+decideOr l r s = case (l s, r s) of
+    (Proved t1, _)  -> Proved (Or'L t1)
+    (_, Proved t2)  -> Proved (Or'R t2)
+    (Disproved a1, Disproved a2) -> Disproved $ \case
+        Or'L pa -> a1 pa
+        Or'R pb -> a2 pb
+
+-- d
+knockableNotOpened
+    :: forall s. SingI s
+    => Refuted (And' Knockable ((:~:) 'Opened) s)
+knockableNotOpened (And' knockable eqOpened) = case knockable of
+    KnockClosed -> case eqOpened of {}
+    KnockLocked -> case eqOpened of {}
+-- knockableNotOpened (And' knockable eqOpened) = case sing @s of
+--     SOpened -> case knockable of {}
+--     SClosed -> case eqOpened of {}
+--     SLocked -> case eqOpened of {}
+
+knockableOrOpened
+    :: forall s. SingI s
+    => Or' Knockable ((:~:) 'Opened) s
+knockableOrOpened = case sing @s of
+    SOpened -> Or'R Refl
+    SClosed -> Or'L KnockClosed
+    SLocked -> Or'L KnockLocked
+
+-- 4.
+knockedRefute
+    :: forall s. SingI s
+    => Knockable s
+    -> Refuted (s :~: 'Opened)
+knockedRefute = \case
+    KnockClosed -> \case {}
+    KnockLocked -> \case {}
+
+refuteKnocked
+    :: forall s. SingI s
+    => Refuted (s :~: 'Opened)
+    -> Knockable s
+refuteKnocked f = case sing @s of
+    SOpened -> absurd $ f Refl
+    SClosed -> KnockClosed
+    SLocked -> KnockLocked
+
+-- 5.
+knockRefl :: (StatePass s :~: 'Obstruct) -> Door s -> IO ()
+knockRefl _ d = putStrLn $ "Knock knock on " ++ doorMaterial d ++ " door!"
+
+knockSomeDoorRefl
+    :: SomeDoor
+    -> IO ()
+knockSomeDoorRefl (MkSomeDoor s d) = case sStatePass s %~ SObstruct of
+    Proved k        -> knockRefl k d
+    Disproved _     -> putStrLn $ "Can't knock on an " ++ show (doorStatus' s d) ++ " door"
+
+-- 6. ???????????
+$(singletons [d|
+    invertPass :: Pass -> Pass
+    invertPass Obstruct = Allow
+    invertPass Allow    = Obstruct
+    |])
+
+knockInv :: (InvertPass (StatePass s) ~ 'Allow) => Door s -> IO ()
+knockInv d = putStrLn $ "Knock knock on " ++ doorMaterial d ++ " door!"
+
+knockSomeDoorInv
+    :: SomeDoor
+    -> IO ()
+knockSomeDoorInv (MkSomeDoor s d) = case sInvertPass (sStatePass s) of
+    -- SAllow :: SPass (InvertPass (StatePass s))
+    SAllow      -> knockInv d
+    SObstruct   -> putStrLn $ "Can't knock on an " ++ show (doorStatus' s d) ++ " door"
+
+-- 7. Kein Bock mehr, erscheint mir ziemlich sinnlos
+
+{--------------------------------------------------------------------------------------------------}
+-- Part 4: https://blog.jle.im/entry/introduction-to-singletons-4.html
+
+$(singletons [d|
+    mergeState :: DoorState -> DoorState -> DoorState
+    mergeState = max
+    |])
+
+mergeDoor
+    :: Door s
+    -> Door t
+    -> Door (MergeState s t)
+mergeDoor d e = UnsafeMkDoor $ doorMaterial d ++ " and " ++ doorMaterial e
+
+mergeSomeDoor :: SomeDoor -> SomeDoor -> SomeDoor
+mergeSomeDoor (MkSomeDoor s1 d1) (MkSomeDoor s2 d2) =
+    MkSomeDoor (sMergeState s1 s2) (mergeDoor d1 d2)
+
+data Hallway :: [DoorState] -> Type where
+    HEnd  :: Hallway '[]        -- ^ end of the hallway, a stretch with no
+                                --   doors
+    (:<#) :: Door s
+          -> Hallway ss
+          -> Hallway (s ': ss)  -- ^ A door connected to a hallway is a new
+                                --   hallway, and we track the door's state
+                                --   in the list of hallway door states
+
+deriving instance Show (Hallway ss)
+
+infixr 5 :<#
+
+myHallway :: Hallway '[ 'Closed, 'Opened, 'Locked]
+myHallway = let
+    door1 = mkDoor SClosed "Oak"
+    door2 = mkDoor SOpened "Spruce"
+    door3 = mkDoor SLocked "Acacia"
+    in door1 :<# door2 :<# door3 :<# HEnd
+
+$(singletons [d|
+    mergeStateList :: [DoorState] -> DoorState
+    mergeStateList []     = Opened
+    mergeStateList (s:ss) = s `mergeState` mergeStateList ss
+    |])
+-- mergeStateList = foldr mergeState Opened
+-- mergeStateList []     = Opened
+-- mergeStateList (s:ss) = s `mergeState` mergeStateList ss
+
+collapseHallway :: Hallway ss -> Door (MergeStateList ss)
+collapseHallway HEnd        = mkDoor SOpened "Air"
+collapseHallway (x :<# xs)  = mergeDoor x (collapseHallway xs)
+
+{-
+When i define
+    mergeStateList = foldr mergeState Opened
+then i get an error on the line
+    collapseHallway (x :<# xs)  = mergeDoor x (collapseHallway xs)
+The error is completely unreadable:
+    • Could not deduce: Data.Singletons.Prelude.Ord.Case_6989586621679865199
+                          s
+                          (Data.Singletons.Prelude.Base.Let6989586621680020518Go
+                             MergeStateSym0 'Opened ss1 ss1)
+                          (Data.Singletons.Prelude.Ord.Case_6989586621679865151
+                             s
+                             (Data.Singletons.Prelude.Base.Let6989586621680020518Go
+                                MergeStateSym0 'Opened ss1 ss1)
+                             (Compare_6989586621679053359
+                                s
+                                (Data.Singletons.Prelude.Base.Let6989586621680020518Go
+                                   MergeStateSym0 'Opened ss1 ss1)))
+                        ~ Data.Singletons.Prelude.Ord.Case_6989586621679865199
+                            s
+                            (Data.Singletons.Prelude.Base.Let6989586621680020518Go
+                               MergeStateSym0 'Opened (s : ss1) ss1)
+                            (Data.Singletons.Prelude.Ord.Case_6989586621679865151
+                               s
+                               (Data.Singletons.Prelude.Base.Let6989586621680020518Go
+                                  MergeStateSym0 'Opened (s : ss1) ss1)
+                               (Compare_6989586621679053359
+                                  s
+                                  (Data.Singletons.Prelude.Base.Let6989586621680020518Go
+                                     MergeStateSym0 'Opened (s : ss1) ss1)))
+      from the context: ss ~ (s : ss1)
+-}
+
+myCollapsedHallway :: Door 'Locked
+myCollapsedHallway = collapseHallway myHallway
